2461d6ee361b33e9501170a22be95bc57568ce9f
Gabriela Nery <gneri94@gmail.com>
2018-05-29 16:22:49 -0300

    conexao com banco

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,8 +1,18 @@
 <?php
+
+    require_once(db.class.php);
+
     $_POST['usuario'];
     echo $_POST['usuario'];
     $_POST['email'];
     echo $_POST['email'];
     $_POST['senha'];
     echo $_POST['senha'];
+
+    $objDb = new db();
+    $link = $objDb->conecta_mysql;
+
+ 
+
+
 ?>
