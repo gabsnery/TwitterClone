1160836834fa5716507c9264e43122bf83ee3d23
gabsnery <gneri94@gmail.com>
2018-06-01 12:26:15 -0300

    session

--- verify_access.php
+++ verify_access.php
@@ -1,15 +1,15 @@
 <?php
+    session_start();
+    require_once('db.class.php');
+
     //o post pega a partir do atributo NAME
     $password = $_POST['password'];
     $user = $_POST['user'];
 
-    require_once('db.class.php');
-
     $objDB = new db();
     $link = $objDB->conecta_mysql();
 
-    $sql = "select * from users where user = '$user' and password = '$password'";
-
+    $sql = "select user,email,id from users where user = '$user' and password = '$password'";
 
     //RETORNOS DO MYSQLI
     //UPDATE TRUE/FALSE
@@ -18,13 +18,16 @@
     //SELECT FALSE CASO EXISTA PROBLEMA, E UMA REFERENCIA CASO A QUERY SEJA VALIDA
     $result_sql_id = mysqli_query($link,$sql);
 
-
     if ($result_sql_id){
 
         $data_user = mysqli_fetch_array($result_sql_id);
 
         if (isset($data_user['user'])){
-            echo "Usuário existe";
+            $_SESSION['user'] = $data_user['user'];
+            $_SESSION['id'] = $data_user['id'];
+            $_SESSION['email'] = $data_user['email'];
+            
+            header('Location :home.php');
         }else{
             header('Location :index.php?erro=1');
         }
@@ -32,10 +35,4 @@
     }else{
         echo "Erro na execução da consulta, favor entrar em contato com o admin do site!";
     }
-
-
-
-    
-
-
 ?>
