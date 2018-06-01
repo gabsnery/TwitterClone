22d9a56ff21e2fa2ccb2fa8453a223b18c1df9e1
gabsnery <gneri94@gmail.com>
2018-06-01 20:55:39 -0300

    Validar e-mail e usuario no registro

--- registra_usuario.php
+++ registra_usuario.php
@@ -8,37 +8,46 @@
 
     $objDb = new db();
     $link = $objDb->conecta_mysql();
-
+ //TESTA SE EXISTE CONTA COM O usuario INFORMADO
     $sql = "select * from users where user = '$user'";
 
     if($result_id = mysqli_query($link,$sql)){
 
         $data_user = mysqli_fetch_array($result_id);
-
         if ( $data_user['user'] ){
             echo "Usuário já cadastrado!";
-            die();
+            $usuario_existe = 1;
         }
     }else{
         echo "Erro ao localizar o registro do usuário!";
     }
-   
 
     //TESTA SE EXISTE CONTA COM O EMAIL INFORMADO
     $sql = "select * from users where email = '$email'";
-
     if($result_id = mysqli_query($link,$sql)){
-
         $data_user = mysqli_fetch_array($result_id);
-
         if ( $data_user['email'] ){
             echo "Usuário já cadastrado para este e-mail!";
-            die();
+            $email_existe = 1;
         }
     }else{
         echo "Erro ao localizar o registro do usuário!";
     }
 
+    if(($email_existe == 1) || ($usuario_existe == 1)){
+
+        $retorno_get = '';
+        if($email_existe){
+            $retorno_get.= "erro_email=".$email_existe."&";
+        }
+        if($usuario_existe){
+            $retorno_get.= "erro_usuario=".$usuario_existe;
+        }
+    
+       header('Location: inscrevase.php?'.$retorno_get);
+       die();
+    }
+    
     $sql = "INSERT INTO users (user, email, password) VALUES ('$user', '$email', '$password')";
     //executar a query
     if(mysqli_query($link, $sql)){
