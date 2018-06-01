4161e13d10f4713c34b9f5b838f5db692ab57986
gabsnery <gneri94@gmail.com>
2018-06-01 19:26:41 -0300

    Validar e-mail e usuario no registro

--- registra_usuario.php
+++ registra_usuario.php
@@ -9,7 +9,35 @@
     $objDb = new db();
     $link = $objDb->conecta_mysql();
 
+    $sql = "select * from users where user = '$user'";
 
+    if($result_id = mysqli_query($link,$sql)){
+
+        $data_user = mysqli_fetch_array($result_id);
+
+        if ( $data_user['user'] ){
+            echo "Usuário já cadastrado!";
+            die();
+        }
+    }else{
+        echo "Erro ao localizar o registro do usuário!";
+    }
+   
+
+    //TESTA SE EXISTE CONTA COM O EMAIL INFORMADO
+    $sql = "select * from users where email = '$email'";
+
+    if($result_id = mysqli_query($link,$sql)){
+
+        $data_user = mysqli_fetch_array($result_id);
+
+        if ( $data_user['email'] ){
+            echo "Usuário já cadastrado para este e-mail!";
+            die();
+        }
+    }else{
+        echo "Erro ao localizar o registro do usuário!";
+    }
 
     $sql = "INSERT INTO users (user, email, password) VALUES ('$user', '$email', '$password')";
     //executar a query
