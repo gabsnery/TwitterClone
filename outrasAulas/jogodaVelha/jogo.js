dcaf72c63688f189d1923b13b5d2bb9ad537f9a2
gabsnery <gneri94@gmail.com>
2018-08-29 21:01:07 -0300

    nomear jogadores

--- outrasAulas/jogodaVelha/jogo.js
+++ outrasAulas/jogodaVelha/jogo.js
@@ -1,5 +1,17 @@
 $(document).ready(function (){
     $('#btn_iniciar_jogo').click(function(){
-        alert($('#entrada_apelido_jogador_2').val());
+
+        if ($('#entrada_apelido_jogador_1').val() == ''){
+            alert('Apelido do jogador 1 não foi preenchido');
+            return false;
+        }
+        if ($('#entrada_apelido_jogador_2').val() == ''){
+            alert('Apelido do jogador 2 não foi preenchido');
+            return false;
+        }
+
+        $('#nome_jogador_1').html($('#entrada_apelido_jogador_1').val());
+        $('#nome_jogador_2').html($('#entrada_apelido_jogador_2').val());
     });
+
 });
