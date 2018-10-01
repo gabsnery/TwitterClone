b50754833dceb0d8062c86f702e293806183b079
gabsnery <gneri94@gmail.com>
2018-10-01 15:32:22 -0300

    require

---
+++ outrasAulas/aulasPHP/index.php
@@ -0,0 +1,6 @@
+
+<?php
+    include "menu.php";
+    //include_once "menu.php";  COMANDO PERCEBE SE O ARQUIVO JÁ FOI USADO ANTES
+?>
+Index <br>
---
+++ outrasAulas/aulasPHP/tecnologia.php
@@ -0,0 +1,5 @@
+<?php
+    include "menu.php";
+    //include_once "menu.php";  COMANDO PERCEBE SE O ARQUIVO JÁ FOI USADO ANTES
+?>
+Tecnologia <br>
