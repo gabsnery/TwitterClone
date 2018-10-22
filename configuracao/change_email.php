effe81f52db86c2808a15a657a33504bf2be870a
Gabriela Nery <gneri94@gmail.com>
2018-10-22 14:54:37 -0300

    Revert "configurar-email"
    This reverts commit ea7a26b49b9736f143c0f7324c0067ef7eabb544.

--- configuracao/change_email.php
+++ configuracao/change_email.php
@@ -1,17 +1,27 @@
-<?php
-    session_start();
+<?php        
+        session_start();
 
-    if (!isset($_SESSION['user'])){
-        header('Location : index.php?erro=2');
-     }
+        $newUser = $_POST['email_novo'];
+    
+         if (!isset($_SESSION['user'])){
+            header('Location : index.php?erro=2');
+         }
+    
+         require_once('db.class.php');
+    
+         $id_user = $_SESSION['id'];
 
-     require_once('..\db.class.php');
+         $ObjDB = new db();
+         $link = $ObjDB->connecta_mysql();
 
-     $id_user = $_SESSION['id'];
-     $newEmail = $_POST['novo_email'];
-     $teste = $_POST['teste'];
-     echo $teste;
+         $sql = "update users set user = '".$newUser."' where id=".$id_user;
 
+         $result_sql_id = mysqli_query($link,$sql);
 
+         if($result_sql_id){
+            echo " Usuário alterado com sucesso";
+         }else{
+            echo " Erro ao alterar usuario";
+         }
 
 ?>
